7f4ccf036880fa17ee03b3c97c4468040c7412ea
Huy NGUYEN <huu_nhat_huy.nguyen@insa-cvl.fr>
2024-11-14 12:07:55 +0100

    Final version

--- main-Huy.py
+++
@@ -1,9 +0,0 @@
-from Fonction_laser import *
-
-list_cameras, message = list_ports_camera()
-
-print(message)
-print("Nombre de camera detecte: ",len(list_cameras))
-
-# Smile! You're on camera
-save_image(0)
--- main-Khoa.py
+++
@@ -1,5 +0,0 @@
-from Fonction_laser import *
-
-camera_VideoCapture()
-
-camera_connection()
